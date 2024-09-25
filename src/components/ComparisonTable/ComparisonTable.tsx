75fc34638c14b48b7dde2e48b369d1fc84fd90f9
yana ost <yanaost@yanas-Laptop.local>
2024-09-25 20:43:59 +1000

    started to build accordion tables

--- src/components/ComparisonTable/ComparisonTable.tsx
+++ src/components/ComparisonTable/ComparisonTable.tsx
@@ -1,43 +1,9 @@
-import { Box, Collapse, IconButton, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-import { DeleteOutline, ExpandLess, ExpandMore } from "@mui/icons-material";
-import { useState } from "react";
+import { DeleteOutline } from "@mui/icons-material";
 import { allStrollers } from "../../data/allStrollers";
-
-const StyledTableRow = styled("div")(({ theme }) => ({
-  padding: "16px 0",
-  "&:nth-of-type(even)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-}));
-
-const StyledTableRowTitle = styled("h3")(({ theme }) => ({
-  marginBottom: 8,
-  marginBlockStart: 0,
-  fontSize: "1rem",
-  lineHeight: 1.35,
-  letterSpacing: ".01rem",
-  flexGrow: 0,
-  flexShrink: 0,
-  paddingRight: 4,
-  paddingLeft: 4,
-  flexBasis: "100%",
-  maxWidth: "100%",
-
-  [theme.breakpoints.up("sm")]: {
-    paddingRight: 8,
-    paddingLeft: 8,
-  },
-
-  [theme.breakpoints.up("lg")]: {
-    marginTop: 32,
-    paddingRight: 8,
-    paddingLeft: 8,
-    flexBasis: "25%",
-    maxWidth: "25%",
-  },
-}));
+// import { AccordionTable } from "../AccordionTable/AccordionTable";
 
 const StyledTableFirstHeadCell = styled(Box)(({ theme }) => ({
   display: "none",
@@ -95,43 +61,6 @@ const StyledTableCellContainer = styled("div")(({ theme }) => ({
     flexShrink: 0,
     flexBasis: "25%",
     maxWidth: "25%",
-  },
-}));
-
-const StyledTableCell = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  width: "100%",
-  flexGrow: 0,
-  flexShrink: 0,
-  paddingRight: 4,
-  paddingLeft: 4,
-  flexBasis: "50%",
-  maxWidth: "50%",
-  [theme.breakpoints.up("sm")]: {
-    paddingRight: 8,
-    paddingLeft: 8,
-    flexGrow: 0,
-    flexShrink: 0,
-    flexBasis: "50%",
-    maxWidth: "50%",
-  },
-  [theme.breakpoints.up("md")]: {
-    paddingTop: 32,
-    paddingBottom: 32,
-    flexBasis: "33.33%",
-    maxWidth: "33.33%",
-  },
-
-  [theme.breakpoints.up("lg")]: {
-    paddingTop: 32,
-    paddingBottom: 32,
-    paddingRight: 8,
-    paddingLeft: 8,
-    flexGrow: 0,
-    flexShrink: 0,
-    flexBasis: "33%",
-    maxWidth: "33%",
   },
 }));
 
@@ -202,15 +131,6 @@ const SectionContainer = styled(Box)(({ theme }) => ({
   },
 }));
 
-const SectionContainerShadow = styled("div")(() => ({
-  display: "flex",
-  boxShadow: "rgba(17, 17, 26, 0.1) 0px 1px 0px",
-  justifyContent: "space-between",
-  height: "60px",
-  alignItems: "center",
-  width: "100%",
-}));
-
 const Section = styled("section")(({ theme }) => ({
   display: "flex",
   flex: " 0 1 auto",
@@ -227,17 +147,6 @@ const Section = styled("section")(({ theme }) => ({
     marginRight: -8,
     marginLeft: -8,
     paddingTop: 24,
-  },
-}));
-
-const StyledTableRowWrapContent = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexBasis: "100%",
-  flexWrap: "nowrap",
-
-  [theme.breakpoints.up("lg")]: {
-    flexBasis: "75%",
-    maxWidth: "75%",
   },
 }));
 
@@ -343,36 +252,10 @@ const ProductDescriptionListItem = styled("li")(() => ({
   padding: 0,
 }));
 
-const AccordionContainer = styled("div")(() => ({
-  paddingTop: 20,
-}));
-
-const AccordionTitle = styled("h2")(() => ({
-  fontSize: `calc(20px + 4*(100vw - 480px) / 760)`,
-}));
-
 type Props = {
   strollersIdsToCompare: number[];
   handleDeleteStrollerIdFromCompare: (strollerId: number) => void;
 };
-
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 
 export const ComparisonTable: React.FC<Props> = ({
   strollersIdsToCompare,
@@ -388,7 +271,29 @@ export const ComparisonTable: React.FC<Props> = ({
 
   const strollersDataToShow = getStrollersDataToShow(strollersIdsToCompare);
 
-  const [isOpen, setIsOpen] = useState(true);
+  //prepare data for accordion
+  //from this format: [
+  //     {group: {key: "basics", title: "the basics"}, fields: [{key, title, value}, {key, title, value}]}
+  // ]
+
+  // final result ==>>>
+  //[
+  //  {
+  // rowTitle: {}
+  // rows: [{ "stroller weight", "7.9", "8.9", "20"},{ "stroller weight", "7.9", "8.9", "20"},]
+  //  },
+  //
+  //]
+
+  console.log("strollersDataToShow", strollersDataToShow);
+
+  const modifyStrollersDataForAccordions = (ids: number[]) => {
+    let filteredData = ids.map((id) =>
+      allStrollers.find((stroller) => stroller.id === id)
+    );
+
+    return filteredData;
+  };
 
   return (
     <ComparisonContainer>
@@ -454,75 +359,9 @@ export const ComparisonTable: React.FC<Props> = ({
         </Section>
       </SectionContainer>
 
-      <AccordionContainer sx={{ marginTop: "20px" }}>
-        <SectionContainer
-          display="flex"
-          justifyContent="space-between"
-          height="60px"
-          alignItems="center"
-        >
-          <SectionContainerShadow>
-            <AccordionTitle>The Basics</AccordionTitle>
-            <IconButton onClick={() => setIsOpen(!isOpen)}>
-              {!isOpen ? <ExpandMore /> : <ExpandLess />}
-            </IconButton>
-          </SectionContainerShadow>
-        </SectionContainer>
-        <Collapse in={!!isOpen}>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <SectionContainer>
-                <Section sx={{ "&&": { padding: "0px" } }}>
-                  <StyledTableRowTitle sx={{ fontWeight: 600 }}>
-                    {row.name}
-                  </StyledTableRowTitle>
-                  <StyledTableRowWrapContent>
-                    <StyledTableCell>{row.calories}</StyledTableCell>
-                    <StyledTableCell>{row.fat}</StyledTableCell>
-                    <StyledTableCell>{row.carbs}</StyledTableCell>
-                  </StyledTableRowWrapContent>
-                </Section>
-              </SectionContainer>
-            </StyledTableRow>
-          ))}
-        </Collapse>
-      </AccordionContainer>
+      {/* sx={{ marginTop: "20px" }} */}
 
-      <AccordionContainer>
-        <SectionContainer
-          display="flex"
-          justifyContent="space-between"
-          height="60px"
-          alignItems="center"
-        >
-          <SectionContainerShadow>
-            <AccordionTitle>The Seat</AccordionTitle>
-            <IconButton onClick={() => setIsOpen(!isOpen)}>
-              {isOpen ? <ExpandMore /> : <ExpandLess />}
-            </IconButton>
-          </SectionContainerShadow>
-        </SectionContainer>
-        <Collapse>
-          {rows.map((row) => (
-            <StyledTableRow key={row.name}>
-              <SectionContainer>
-                <Section>
-                  <StyledTableRowTitle sx={{ fontWeight: 600 }}>
-                    {row.name}
-                  </StyledTableRowTitle>
-                  <StyledTableCellContainer>
-                    {row.calories}
-                  </StyledTableCellContainer>
-                  <StyledTableCellContainer>{row.fat}</StyledTableCellContainer>
-                  <StyledTableCellContainer>
-                    {row.carbs}
-                  </StyledTableCellContainer>
-                </Section>
-              </SectionContainer>
-            </StyledTableRow>
-          ))}
-        </Collapse>
-      </AccordionContainer>
+      {/* {strollersDataToShow.length > 0 && <AccordionTable />} */}
     </ComparisonContainer>
   );
 };
